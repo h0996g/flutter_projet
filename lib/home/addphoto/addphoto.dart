f566f6ef178ab02dc60b7dfcc9ef7d538df401ec
unknown <houssam.guechi@univ-constantine2.dz>
2022-03-31 21:33:25 +0000

    ;

---
+++ lib/home/addphoto/addphoto.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Addphoto extends StatefulWidget {
+  const Addphoto({Key? key}) : super(key: key);
+
+  @override
+  State<Addphoto> createState() => _AddphotoState();
+}
+
+class _AddphotoState extends State<Addphoto> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
