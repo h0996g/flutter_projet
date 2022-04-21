61d055c13536b86da5cb731c08530c2416c3a701
unknown <houssam.guechi@univ-constantine2.dz>
2022-04-21 16:02:05 +0100

    constapi

--- lib/Api/constApi.dart
+++ lib/Api/constApi.dart
@@ -4,5 +4,5 @@ const LOGOUT = '/api/remove';
 const USERINFOTOKEN = '/api/user';
 const REGISTERCLIENT = '/api/registerclient';
 const REGISTERAGENCE = '/api/registeragence';
-const URLHTTP = '192.168.1.5:8000';
+const URLHTTP = '192.168.55.68:8000';
 var TOKEN = '';
